488dd851d2411cf60f4aacbe37a1ae5c2fd73146
skapoor68 <skapoor68@gatech.edu>
2022-05-28 12:41:17 -0400

    change font-size to fontSize

--- components/Property.jsx
+++ components/Property.jsx
@@ -18,7 +18,7 @@ const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, 
         <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
           <Flex alignItems='center'>
             <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
-            <Text fontWeight='bold' font-size='lg'>AED {millify(price)} {rentFrequency && `/ ${rentFrequency}`}</Text>
+            <Text fontWeight='bold' fontSize='lg'>AED {millify(price)} {rentFrequency && `/ ${rentFrequency}`}</Text>
           </Flex>
           <Box>
             <Avatar size='md' src={agency?.logo?.url} />
