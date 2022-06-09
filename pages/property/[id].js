16457ead1a22f194aed2011d8d9366293cc551a7
skapoor68 <skapoor68@gatech.edu>
2022-06-09 12:02:12 -0400

    delete unused imports

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,6 +1,5 @@
-import { Box, Flex, Spacer, Text, Avatar } from '@chakra-ui/react';
+import { Box, Flex, Text, Avatar } from '@chakra-ui/react';
 import { FaBed, FaBath } from 'react-icons/fa';
-import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import { BsRulers } from 'react-icons/bs';
 import millify from 'millify';
