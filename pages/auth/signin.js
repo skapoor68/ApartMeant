50b06275fe8c2249ba07f10b05771a884949451a
skapoor68 <skapoor68@gatech.edu>
2022-06-24 22:03:10 -0400

    begin styling login page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,16 +1,26 @@
 import { getProviders, signIn } from "next-auth/react"
+import { Flex, Box, Button } from '@chakra-ui/react'
 
 export default function SignIn({ providers }) {
   return (
-    <>
+    // <>
+    //   {Object.values(providers).map((provider) => (
+    //     <div key={provider.name}>
+    //       <button onClick={() => signIn(provider.id)}>
+    //         Sign in with {provider.name}
+    //       </button>
+    //     </div>
+    //   ))}
+    // </>
+    <Flex justifyContent='center'>
       {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
+        <Box key={provider.name}>
+          <Button onClick={() => signIn(provider.id)}>
             Sign in with {provider.name}
-          </button>
-        </div>
+          </Button>
+        </Box>
       ))}
-    </>
+    </Flex>
   )
 }
 
